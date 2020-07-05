5000aba37e3f19a13e179b2be3f7d6e2b741b84f
devjony <jonatamicael@gmail.com>
2020-07-05 03:28:17 -0300

    Adiciona querys personalizadas

--- src/main/java/br/com/javaBackendJuniorJonataMicael/repository/ColaboradorRepository.java
+++ src/main/java/br/com/javaBackendJuniorJonataMicael/repository/ColaboradorRepository.java
@@ -1,11 +1,18 @@
 package br.com.javaBackendJuniorJonataMicael.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import br.com.javaBackendJuniorJonataMicael.domain.Colaborador;
+import br.com.javaBackendJuniorJonataMicael.model.Colaborador;
 
 @Repository
 public interface ColaboradorRepository extends JpaRepository<Colaborador, Integer> {
-
+	
+	@Query(value = "SELECT COUNT(*) FROM colaborador", nativeQuery = true)
+	public Integer recuperaTotalColaboradores();
+	
+	@Query(value = "SELECT COUNT(*) FROM colaborador WHERE YEAR(FROM_DAYS(TO_DAYS(NOW())-TO_DAYS(data_nascimento))) > :idade", nativeQuery = true)
+	public Integer recuperaTotalColaboradoresIdadeMax(@Param("idade") Integer idade);
 }
--- src/main/java/br/com/javaBackendJuniorJonataMicael/repository/SetorRepository.java
+++ src/main/java/br/com/javaBackendJuniorJonataMicael/repository/SetorRepository.java
@@ -3,7 +3,7 @@ package br.com.javaBackendJuniorJonataMicael.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.com.javaBackendJuniorJonataMicael.domain.Setor;
+import br.com.javaBackendJuniorJonataMicael.model.Setor;
 
 @Repository
 public interface SetorRepository extends JpaRepository<Setor, Integer> {
